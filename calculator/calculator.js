e76c7f3dcd7bd4e0fdfe4ba7394fb43ef3c2d21b
Patryk Kozlowski <pamelus@users.noreply.github.com>
2021-08-17 10:08:16 +0200

    SOrted out issues with file

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,7 +1,9 @@
-const { read } = require('fs');
-
 function calculator(number1, number2, operator) {
-
+    // Zaimplementujcie proszę tą funckję w taki sposób, aby zwracała wynik działania matematycznego 
+    // pomiędzy dwoma przekazanymi liczbami. Działanie określa argument "operator".
+    // 
+    // Operacja matematyczna powinna byc realizowna przez osobna funkcje, musicie wiec napisac funkcje realizujace
+    // dodawania, odejmowanie, mnozenie i dzielenie, a nastepnie wywołać je w tej funkcji w zaleznosci od przekazanego operatora.
 }
 
 
